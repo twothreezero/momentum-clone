815e9d5c50ac0c94cbab83c206ec82d18f2fbfcf
twothreezero <realtwothreezero@gmail.com>
2025-06-03 17:35:28 +0900

    Add: input username as login-form

---
+++ app.js
@@ -0,0 +1,16 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+const HIDDEN_CLASSNAME = "hidden";
+
+function onLoginSubmit(e) {
+  e.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+
+  console.log(username);
+}
+
+loginForm.addEventListener("submit", onLoginSubmit)
